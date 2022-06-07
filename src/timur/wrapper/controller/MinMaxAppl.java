eb89805e3fe96c3d85e48d3d318065385d9dfa60
TimurMP <titimurmamyshev@gmail.commur>
2022-06-08 00:56:18 +0300

    1

--- src/timur/wrapper/controller/MinMaxAppl.java
+++ src/timur/wrapper/controller/MinMaxAppl.java
@@ -7,64 +7,46 @@ public class MinMaxAppl {
             for (int i = 0; i < arr.length; i++) {
                 System.out.println(maxAndMin(arr[i]) + "\n");
             }
-
+        } else {
+            for (int i = 0; i < args.length; i++) {
+                System.out.println(maxAndMin(args[i]) + "\n");
+            }
         }
-
-
     }
 
 
     public static String maxAndMin(String dataType) {
-        String str;
+        String templateMin = dataType + "'s Min. value is ";
+        String templateMax = "\n" + dataType +"'s Max. value is ";
+
         switch (dataType.toLowerCase()) {
             case "byte":
-                str = "Byte's Min. value is " + Byte.MIN_VALUE
-                        + "\nByte's Max. value is " + Byte.MAX_VALUE;
-                return str;
-
+                return templateMin + Byte.MIN_VALUE + templateMax + Byte.MAX_VALUE;
             case "int":
-                str = "Integer's Min. value is " + Integer.MIN_VALUE
-                        + "\nInteger's Max. value is " + Integer.MAX_VALUE;
-                return str;
-
+                return templateMin + Integer.MIN_VALUE + templateMax + Integer.MAX_VALUE;
             case "short":
-                str = "Short's Min. value is " + Short.MIN_VALUE
-                        + "\nShort's Max. value is " + Short.MAX_VALUE;
-                return str;
-
+                return templateMin + Short.MIN_VALUE + templateMax + Short.MAX_VALUE;
             case "long":
-                str = "Long's Min. value is " + Long.MIN_VALUE
-                        + "\nLong's Max. value is " + Long.MAX_VALUE;
-                return str;
-
+                return templateMin + Long.MIN_VALUE + templateMax + Long.MAX_VALUE;
             case "char":
-                str = "Char's Min. value is " + (int) Character.MIN_VALUE
-                        + "\nChar's Max. value is " + (int) Character.MAX_VALUE;
-                return str;
-
+                return templateMin + (int) Character.MIN_VALUE + templateMax + (int) Character.MAX_VALUE;
             case "float":
-                str = "Float's Min. value is " + Float.MIN_VALUE
-                        + "\nFloat's Max. value is " + Float.MAX_VALUE;
-                return str;
-
+                return templateMin + Float.MIN_VALUE + templateMax + Float.MAX_VALUE;
             case "double":
-                str = "Double's Min. value is " + Double.MIN_VALUE
-                        + "\nDouble's Max. value is " + Double.MAX_VALUE;
-                return str;
-
+                return templateMin + Double.MIN_VALUE + templateMax + Double.MAX_VALUE;
         }
-
         return dataType + " - Wrong Type";
     }
-}
 
 
-//    public static String maxAndMin (String dataType){
-//        String type = dataType.substring(0, 1).toUpperCase() + dataType.substring(1).toLowerCase();/
+//    public static String maxAndMina(String dataType) {
+//        String type = dataType.substring(0, 1).toUpperCase() + dataType.substring(1).toLowerCase();
 //
 //        String str = "Byte's Min. value is " + type.MIN_VALUE
-//        + "Byte's Max. value is " + type.MAX_VALUE;
+//                + "Byte's Max. value is " + type.MAX_VALUE;
 //
 //
 //        return dataType;
 //    }
+
+}
